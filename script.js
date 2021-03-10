0eec731a477b9cbcda9516df7f43445c5a6dbcae
DanielSoaresdev <jdanielsoares097@gmail.com>
2021-03-09 23:29:40 -0300

    Commit Alteração no Menu

---
+++ script.js
@@ -0,0 +1,5 @@
+var iconemenu = document.querySelector(".iconemenu");
+
+iconemenu.addEventListener("click", function(){
+    document.querySelector(".menulateral").classList.toggle("show-menu");
+});
